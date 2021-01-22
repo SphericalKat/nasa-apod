2c42cd0bcd587f638251339c0d8847c5480a07e5
SphericalKat <amolele@gmail.com>
2021-01-22 16:41:30 +0530

    chore(di): prepare for DI using Hilt
    Signed-off-by: SphericalKat <amolele@gmail.com>

---
+++ app/src/main/java/dev/smoketrees/nasa_apod/App.kt
@@ -0,0 +1,7 @@
+package dev.smoketrees.nasa_apod
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
--- app/src/main/java/dev/smoketrees/nasa_apod/MainActivity.kt
+++ app/src/main/java/dev/smoketrees/nasa_apod/MainActivity.kt
@@ -2,7 +2,9 @@ package dev.smoketrees.nasa_apod
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
