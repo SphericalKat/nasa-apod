f34265c4c5469a0145f6b7e662724b8fa5ac6902
SphericalKat <amolele@gmail.com>
2021-01-22 16:32:12 +0530

    Initial commit
    Signed-off-by: SphericalKat <amolele@gmail.com>

---
+++ app/src/main/java/dev/smoketrees/nasa_apod/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.smoketrees.nasa_apod
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
